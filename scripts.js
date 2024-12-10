0131292074b2cf2818340b3b7ab01d55aa9e4f96
Bruno Loberto <brunoloberto662@gmail.com>
2024-12-10 10:08:26 -0300

    tesla clone

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let formulário = document.querySelector(".formulário")
+
+function aparecerFormulario() {
+    formulário.style.left = "50%"
+}
+
+function desaparecerFormulario() {
+    formulário.style.left = "-50%"
+}
